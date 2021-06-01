7c794189b0bc2177eb37d5af1f76baecdf780caf
Thiago Negreiros <negreiros.t@gmail.com>
2021-06-01 17:45:56 -0300

    feat: apply stream concept

--- 01-rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ 01-rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,18 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
 export class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    // ? Vai pegar o que ta sendo lido e coloca o que foi lido para algum lugar
+    stream.pipe(parseFile);
+
+    // ? Ele vai receber as nossas linhas
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
