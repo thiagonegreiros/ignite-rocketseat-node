03be463d3a09db05bccc67abe09515209085c635
Thiago Negreiros <negreiros.t@gmail.com>
2021-06-30 23:32:09 -0300

    feat: validate with user is admin

--- 01-rentx/src/shared/infra/http/routes/categories.routes.ts
+++ 01-rentx/src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,9 @@ import { CreateCategoryController } from "@modules/cars/useCases/createCategory/
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const categoriesRoutes = Router();
 const createCategoryController = new CreateCategoryController();
 const importCategoryContoller = new ImportCategoryController();
@@ -13,10 +16,17 @@ const listCategoriesController = new ListCategoriesController();
 const upload = multer({
   dest: "./tmp",
 });
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 categoriesRoutes.get("/", listCategoriesController.handle);
 categoriesRoutes.post(
   "/import",
+  ensureAuthenticated,
+  ensureAdmin,
   upload.single("file"),
   importCategoryContoller.handle
 );
--- 01-rentx/src/shared/infra/http/routes/specifications.routes.ts
+++ 01-rentx/src/shared/infra/http/routes/specifications.routes.ts
@@ -3,10 +3,17 @@ import { Router } from "express";
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+
 const specificarionsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 
-specificarionsRoutes.use(ensureAuthenticated);
-specificarionsRoutes.post("/", createSpecificationController.handle);
+// specificarionsRoutes.use(ensureAuthenticated);
+specificarionsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 export { specificarionsRoutes };
